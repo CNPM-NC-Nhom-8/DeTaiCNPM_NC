3dd45361dce4f476d0f405e14c11364cedab39bf
Asakuri <phungtanphat23@gmail.com>
2023-10-30 16:12:38 +0000

    Lower padding bottom to 2 instead of 4.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
 	return (
-		<main className="container flex max-w-6xl flex-grow flex-col gap-2 py-4">
+		<main className="container flex max-w-6xl flex-grow flex-col gap-2 pb-2 pt-4">
 			<AdminNavbar />
 
 			<section className="flex flex-grow">{children}</section>
