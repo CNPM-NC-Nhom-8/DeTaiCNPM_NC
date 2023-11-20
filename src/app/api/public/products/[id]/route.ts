134217c3c799110fdd0c6cd3afbf5600d6c1f6a3
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2023-11-20 10:37:26 +0700

    Add more more api route for product, pre-compute value for easy access for mobile. (#4)

--- src/app/api/public/products/[id]/route.ts
+++ src/app/api/public/products/[id]/route.ts
@@ -3,7 +3,7 @@ import { db } from "@/server/db";
 import { NextResponse } from "next/server";
 
 export const GET = async (_req: Request, context: { params: { id: string } }) => {
-	const data = await db.sanPhamMau.findFirst({
+	const product = await db.sanPhamMau.findFirst({
 		where: { MaSPM: context.params.id },
 		include: {
 			HinhAnh: true,
@@ -14,5 +14,13 @@ export const GET = async (_req: Request, context: { params: { id: string } }) =>
 		},
 	});
 
-	return NextResponse.json(data);
+	if (!product) return NextResponse.json({ message: "Không tìm thấy sản phẩm" }, { status: 404 });
+
+	const defaultProduct = product.SanPhamBienThe.sort((a, b) => a.Gia - b.Gia).at(0)!;
+
+	return NextResponse.json({
+		...product,
+		GiaGoc: defaultProduct.Gia,
+		SoLuong: defaultProduct.MatHang?.TonKho ?? 0,
+	});
 };
