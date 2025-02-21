0f4272b0abfb956c07812ea6c6b293b57760756f
Noki-Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2025-02-21 12:13:13 +0700

    Update ESLint configuration to disable unbound-method rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,8 +15,9 @@ const config = {
 			"warn",
 			{ prefer: "type-imports", fixStyle: "inline-type-imports" },
 		],
-		"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
 		"@typescript-eslint/require-await": "off",
+		"@typescript-eslint/unbound-method": "off",
+		"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
 		"@typescript-eslint/no-misused-promises": ["error", { checksVoidReturn: { attributes: false } }],
 	},
 };
